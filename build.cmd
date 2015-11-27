fd8feef80686028c6fce22f3bf57926f6d17c263
Joel Brown <joelsbrown1@gmail.com>
2015-11-27 15:33:03 -0700

    use dnx

---
+++ build.cmd
@@ -0,0 +1,34 @@
+@echo off
+cd %~dp0
+
+SETLOCAL
+SET NUGET_VERSION=latest
+SET CACHED_NUGET=%LocalAppData%\NuGet\nuget.%NUGET_VERSION%.exe
+SET BUILDCMD_DNX_FEED=https://www.nuget.org/api/v2
+SET BUILDCMD_DNX_VERSION=1.0.0-rc1-final 
+
+IF EXIST %CACHED_NUGET% goto copynuget
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/%NUGET_VERSION%/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:copynuget
+IF EXIST .nuget\nuget.exe goto sake
+md .nuget
+copy %CACHED_NUGET% .nuget\nuget.exe > nul
+
+:sake
+IF EXIST packages\Sake goto korebuild
+.nuget\NuGet.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -o packages
+
+:korebuild
+IF EXIST packages\Sake goto dnx
+.nuget\nuget.exe install KoreBuild -ExcludeVersion -Source https://www.myget.org/F/aspnetvnext/api/v2 -o packages -nocache -pre
+
+
+:dnx
+SET DNX_FEED=%BUILDCMD_DNX_FEED%
+CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CoreCLR -arch x86
+CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x86 -alias default
+
+packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
--- test/TileProj.Test/SphericalMercatorTests.cs
+++ test/TileProj.Test/SphericalMercatorTests.cs
@@ -1,29 +1,31 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace TileProj.Test
 {
-    [TestClass]
     public class SphericalMercatorTests
     {
-        [TestMethod]
+        [Fact]
         public void SphericalMercator_CoordToEnvelope()
         {
             SphericalMercator sm = new SphericalMercator();
             Coord c = new Coord(0, 0, 0);
             Envelope e = new Envelope(-20037508.342789244, -20037508.342789244, 20037508.342789244, 20037508.342789244);
-            Assert.AreEqual(e, sm.CoordToEnvelope(c));
+            Assert.Equal(e, sm.CoordToEnvelope(c));
 
             Coord c1 = new Coord(1, 0, 1);
             Envelope e1 = new Envelope(-20037508.342789244, -20037508.342789244, 0, 0);
-            Assert.AreEqual(e1, sm.CoordToEnvelope(c1));
+            Assert.Equal(e1, sm.CoordToEnvelope(c1));
 
             Coord c2 = new Coord(1, 0, 0, true);
             Envelope e2 = new Envelope(-20037508.342789244, -20037508.342789244, 0, 0);
-            Assert.AreEqual(e1, sm.CoordToEnvelope(c1));
+            Assert.Equal(e1, sm.CoordToEnvelope(c1));
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_GetChildren()
         {
             SphericalMercator sm = new SphericalMercator();
@@ -33,13 +35,13 @@ namespace TileProj.Test
             Coord sw = new Coord(10, 486, 333);
             Coord se = new Coord(10, 487, 333);
             Coord ne = new Coord(10, 487, 332);
-            Assert.AreEqual(nw, children.NorthWest);
-            Assert.AreEqual(sw, children.SouthWest);
-            Assert.AreEqual(se, children.SouthEast);
-            Assert.AreEqual(ne, children.NorthEast);
+            Assert.Equal(nw, children.NorthWest);
+            Assert.Equal(sw, children.SouthWest);
+            Assert.Equal(se, children.SouthEast);
+            Assert.Equal(ne, children.NorthEast);
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_GetChildren_TMS()
         {
             SphericalMercator sm = new SphericalMercator();
@@ -49,85 +51,84 @@ namespace TileProj.Test
             Coord sw = new Coord(10, 486, 332, true);
             Coord se = new Coord(10, 487, 332, true);
             Coord ne = new Coord(10, 487, 333, true);
-            Assert.AreEqual(nw, children.NorthWest);
-            Assert.AreEqual(sw, children.SouthWest);
-            Assert.AreEqual(se, children.SouthEast);
-            Assert.AreEqual(ne, children.NorthEast);
+            Assert.Equal(nw, children.NorthWest);
+            Assert.Equal(sw, children.SouthWest);
+            Assert.Equal(se, children.SouthEast);
+            Assert.Equal(ne, children.NorthEast);
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_GetParent_MinTile()
         {
             SphericalMercator sm = new SphericalMercator();
             Coord c = new Coord(0, 0, 0);
-            Assert.AreEqual(c, sm.GetParent(c));
+            Assert.Equal(c, sm.GetParent(c));
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_GetParent()
         {
             SphericalMercator sm = new SphericalMercator();
             Coord c = new Coord(10, 486, 332);
             Coord e = new Coord(9, 243, 166);
-            Assert.AreEqual(e, sm.GetParent(c));
+            Assert.Equal(e, sm.GetParent(c));
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_PointToCoord()
         {
             SphericalMercator sm = new SphericalMercator();
             Point p = new Point(-11131949, 4738603);
             Coord e = new Coord(12, 910, 1563);
-            Assert.AreEqual(e, sm.PointToCoord(p, 12));
+            Assert.Equal(e, sm.PointToCoord(p, 12));
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_PointToCoord_MaxExtent()
         {
             SphericalMercator sm = new SphericalMercator();
             Point p1 = new Point(-20037508.342789244, -20037508.342789244);
             Point p2 = new Point(20037508.342789244, 20037508.342789244);
             Coord e1 = new Coord(0, 0, 0);
-            Assert.AreEqual(e1, sm.PointToCoord(p1, 0));
-            Assert.AreEqual(e1, sm.PointToCoord(p2, 0));
+            Assert.Equal(e1, sm.PointToCoord(p1, 0));
+            Assert.Equal(e1, sm.PointToCoord(p2, 0));
             Coord e2 = new Coord(4, 0, 15);
-            Assert.AreEqual(e2, sm.PointToCoord(p1, 4));
+            Assert.Equal(e2, sm.PointToCoord(p1, 4));
             Coord e3 = new Coord(4, 15, 0);
-            Assert.AreEqual(e3, sm.PointToCoord(p2, 4));
+            Assert.Equal(e3, sm.PointToCoord(p2, 4));
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_LongLatToXY()
         {
             SphericalMercator sm = new SphericalMercator();
             Point ll = new Point(-122.33517, 47.63752);
             Point merc = new Point(-13618288.8305, 6046761.54747);
             IPoint actual = sm.LongLatToXY(ll);
-            Assert.AreEqual(merc.X, actual.X, .0001);
-            Assert.AreEqual(merc.Y, actual.Y, .0001);
+            Assert.Equal(merc.X, actual.X, 4);
+            Assert.Equal(merc.Y, actual.Y, 4);
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_LongLatToXY_MaxExtent()
         {
             SphericalMercator sm = new SphericalMercator();
             Point ll = new Point(-180, -90);
             Point merc = new Point(-20037508.342789244, -20037508.342789244);
             IPoint actual = sm.LongLatToXY(ll);
-            Assert.AreEqual(merc.X, actual.X);
-            Assert.AreEqual(merc.Y, actual.Y);
+            Assert.Equal(merc.X, actual.X);
+            Assert.Equal(merc.Y, actual.Y);
         }
-
-        [TestMethod]
+        
+        [Fact]
         public void SphericalMercator_XYToLongLat()
         {
             SphericalMercator sm = new SphericalMercator();
             Point ll = new Point(-122.33517, 47.63752);
             Point merc = new Point(-13618288.8305, 6046761.54747);
             IPoint actual = sm.XYtoLongLat(merc);
-            Assert.AreEqual(ll.X, actual.X, .0001);
-            Assert.AreEqual(ll.Y, actual.Y, .0001);
+            Assert.Equal(ll.X, actual.X, 4);
+            Assert.Equal(ll.Y, actual.Y, 4);
         }
-
     }
 }
